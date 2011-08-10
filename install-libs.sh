4e1d4ccc6b7eebc4e8c10e37b63108164655b105
madhums <madhums8@gmail.com>
2011-08-10 00:33:59 +0000

    libs to install

--- install-libs.sh
+++ install-libs.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-./install-mongodb.sh
-./install-nodejs.sh
 ./install-ror.sh
+./install-nodejs.sh
+./install-mongodb.sh
 
 # mysql
 yes | echo "madhu" | sudo -S apt-get install mysql-server
+yes | echo "madhu" | sudo -S apt-get install libmysqlclient15-dev
+
+# apache2 utils
+yes | echo "madhu" | sudo -S apt-get install apache2-utils
+
+# others
+yes | echo "madhu" | sudo -S apt-get install libxslt-dev libxml2-dev
+
+# install nautilus-elementary theme
+# globus preview
+# remove libreoffice
+# remove banshee
+# install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
+# rvm
+# sphinx
+
