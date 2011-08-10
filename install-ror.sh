a61734343c48e5c689a438ba25d04f30d8d28621
madhums <madhums8@gmail.com>
2011-08-10 00:35:02 +0000

    ror install script changes

--- install-ror.sh
+++ install-ror.sh
@@ -3,8 +3,6 @@
 # Versions of softwares to be installed and paths
 RUBYGEMS=rubygems-1.3.6
 
-source ~/.bashrc
-
 # prerequisites
 yes | echo "madhu" | sudo -S apt-get install aptitude
 
@@ -18,3 +16,8 @@ tar -xzvf $RUBYGEMS.tgz
 cd $RUBYGEMS
 echo "madhu" | sudo -S ruby setup.rb
 echo "madhu" | sudo -S ln -s /usr/bin/gem1.8 /usr/bin/gem
+
+echo 'export GEM_HOME=$HOME/.gem/ruby/1.8' >> ~/.paths
+echo 'export PATH=$HOME/.gem/ruby/1.8/bin:$PATH' >> ~/.paths
+
+source ~/.bashrc
