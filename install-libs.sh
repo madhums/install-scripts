c3ab5129bd080ccadd92185e140caaebbead946a
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-04-27 05:33:42 +0530

    install apache2

--- install-libs.sh
+++ install-libs.sh
@@ -9,6 +9,7 @@ yes | echo "madhu" | sudo -S apt-get install mysql-server
 yes | echo "madhu" | sudo -S apt-get install libmysqlclient15-dev
 
 # apache2 utils
+yes | echo "madhu" | sudo -S apt-get install apache2
 yes | echo "madhu" | sudo -S apt-get install apache2-utils
 
 # others
@@ -24,4 +25,3 @@ yes | echo "madhu" | sudo -S apt-get install libxslt-dev libxml2-dev
 # install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
 # rvm
 # sphinx
-
